d64454d5216e8e040b8c6f9cdef2ab345eab4f81
mtlabdo <abdessamadmaatallaoui@gmail.com>
2024-02-27 10:08:46 +0100

    release 2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vehicle.immatriculation.vin"
         minSdk = 23
         targetSdk = 34
-        versionCode = 6
-        versionName = "2.4"
+        versionCode = 25
+        versionName = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
