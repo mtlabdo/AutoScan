d0cd21d4c846692da61be694b7ddf9e66c3b8abc
mtlabdo <abdessamadmaatallaoui@gmail.com>
2024-02-28 19:59:42 +0100

    release 2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         }
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
