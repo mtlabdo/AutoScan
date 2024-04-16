40731fab2f990f5114f2333a294f8fe0f9a3ed17
mtlabdo <abdessamadmaatallaoui@gmail.com>
2024-04-16 16:18:13 +0200

    2.9.0

--- data/src/main/java/com/vehicle/immatriculation/vin/data/remote/api/ApiInterface.kt
+++ data/src/main/java/com/vehicle/immatriculation/vin/data/remote/api/ApiInterface.kt
@@ -2,13 +2,16 @@ package com.vehicle.immatriculation.vin.data.remote.api
 
 import androidx.annotation.Keep
 import com.vehicle.immatriculation.vin.data.BuildConfig
+import com.vehicle.immatriculation.vin.data.remote.model.ApiResponse
 import com.vehicle.immatriculation.vin.data.remote.model.VehicleDetailNetwork
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.QueryName
 
 interface ApiInterface {
 
@@ -18,8 +21,11 @@ interface ApiInterface {
         @Query("immatriculation") plateNumber: String,
     ): Response<VehicleDetailNetwork?>
 
-
-
+    @Headers("X-Api-Key: 8kFCwDuKmZ35fTqkV8UPVR2xzvZAV6uY","X-Firebase-Appcheck: eyJraWQiOiJYcEhKU0EiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxOjcwODQzNjkyMjA0MTp3ZWI6MTkyNDBlZTc0NDg2N2JmZmM0MTU2OCIsImF1ZCI6WyJwcm9qZWN0c1wvNzA4NDM2OTIyMDQxIiwicHJvamVjdHNcL2ZyLXZlaGljdWxlcyJdLCJwcm92aWRlciI6InJlY2FwdGNoYV92MyIsImlzcyI6Imh0dHBzOlwvXC9maXJlYmFzZWFwcGNoZWNrLmdvb2dsZWFwaXMuY29tXC83MDg0MzY5MjIwNDEiLCJleHAiOjE3MTMyNzAzMzUsImlhdCI6MTcxMzE4MzkzNSwianRpIjoiSXBOemZYQTJ1d0syY1gtMFFiTkJSUmNFbkF4ekdiOFlUTHJmVnBJaDRJRSJ9.RR4jixrERxxqocI-zQDzRYcUevs0b_pkA4NUUd4ks14VsOZg8zJMb9rBoVZqQY-GnyzaVZDAJw5yjN-hug5IjeqqnqVUSzZuGTJ0HDdtkEn5MV5KUz-QgYrO9LYmz_I-sGTldiaSRrhK_sAu0XrLiuB4P2WRmRaVLNepRRatdG5zAiQvjZjfKYc5pgZs2g_Z5Hsef6Jjol_6MVj8lZY_8V_GFN5RjmvVTYFIwlW3R5k1e0hDZTbLjAY1jWttRFLuHJwcF-rsGDk5cE1Weo66fvBJWJnMF9fDnbZT37URS2yBI2FFo4WBpQVCZ91N2E9JSIZuzRXFSgvVgaPRBfZBfSyWsCkhPw3SPd3ySnklLISvunB_HCZ63qWBtUtNFn-oZdDgFBneFKSNGTFFu_95Y25pd4rYGTm5w4wwD8L_5UVN-Q7Gb8X8nwXMxxG-uS6lbDrXS7EOgG7rY2sb_xGWV_JlKDUATkwnRDk0Ih5g0vwj1O7p4D3YegRCcK3pHDgm")
+    @GET("https://api.immatriculation-auto.info/vehicles/{plateNumber}")
+    suspend fun _getDetail(
+        @Path("plateNumber") plateNumber: String,
+        ): Response<ApiResponse?>
 }
 
 interface ApiMailInterface {
@@ -28,7 +34,6 @@ interface ApiMailInterface {
     suspend fun sendFeedBack(
         @Body feed: Mail,
     ): Response<Any?>
-
 }
 
 @Keep
--- data/src/main/java/com/vehicle/immatriculation/vin/data/remote/model/VehicleDetailNetwork.kt
+++ data/src/main/java/com/vehicle/immatriculation/vin/data/remote/model/VehicleDetailNetwork.kt
@@ -2,6 +2,91 @@ package com.vehicle.immatriculation.vin.data.remote.model
 
 import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
+import com.vehicle.immatriculation.vin.model.Detail
+
+@Keep
+data class ApiResponse(
+    @SerializedName("input") val input: String,
+    @SerializedName("short_info") val shortInfo: ShortInfo,
+    @SerializedName("share") val share: String,
+    @SerializedName("tags") val tags: Tags,
+    @SerializedName("user_preferences") val userPreferences: UserPreferences,
+    @SerializedName("content") val content: List<Content>,
+    @SerializedName("ads") val ads: Ads,
+    @SerializedName("message") val message: String
+)
+
+@Keep
+data class ShortInfo(
+    @SerializedName("brand") val brand: String,
+    @SerializedName("model") val model: String,
+    @SerializedName("year") val year: Int
+)
+
+@Keep
+data class Tags(
+    @SerializedName("list") val list: List<TagItem>
+)
+
+@Keep
+data class TagItem(
+    @SerializedName("name") val name: String,
+    @SerializedName("icon") val icon: String,
+    @SerializedName("action") val action: Action
+)
+
+@Keep
+data class Action(
+    @SerializedName("type") val type: String,
+    @SerializedName("value") val value: String
+)
+
+@Keep
+data class UserPreferences(
+    @SerializedName("_type") val type: String,
+    @SerializedName("_id") val id: String,
+    @SerializedName("is_favorite") val isFavorite: Boolean,
+    @SerializedName("note") val note: String
+)
+
+@Keep
+data class Content(
+    @SerializedName("_id") val id: String,
+    @SerializedName("_type") val type: String,
+    val title: String? = null,
+    @SerializedName("title_icon") val titleIcon: String? = null,
+    val fields: List<Field>? = null,
+    val token: String? = null,
+    val subtitle: String? = null,
+    val records: List<Record>? = null,
+    val left: NeighborsItem? = null,
+    val right: NeighborsItem? = null
+)
+
+@Keep
+data class Field(
+    val label: String,
+    val value: String
+)
+
+@Keep
+data class Record(
+    val date: String,
+    val name: String
+)
+
+@Keep
+data class NeighborsItem(
+    val id: String,
+    val brand: String,
+    val model: String,
+    val year: Int
+)
+
+@Keep
+data class Ads(
+    @SerializedName("on_end") val onEnd: Boolean
+)
 
 
 @Keep
@@ -42,3 +127,66 @@ data class DetailNetwork(
     @SerializedName("code_moteur") val code_moteur: String? = null,
     @SerializedName("k_type") val k_type: Int? = null
 )
+
+
+fun ApiResponse.toDetail(): Detail {
+    val content = this.content
+
+    // Finding the content with title "Identification"
+    val identificationContent = content.find { it.title == "Identification" }
+    // Extracting relevant fields from the identification content
+    val immat = identificationContent?.fields?.find { it.label == "Numéro d'immatriculation" }?.value
+    val vin = identificationContent?.fields?.find { it.label == "Numéro d'Identification Véhicule (VIN)" }?.value
+    val Finition = identificationContent?.fields?.find { it.label == "Finition" }?.value
+    val Carrosserie = identificationContent?.fields?.find { it.label == "Carrosserie" }?.value
+    val Type = identificationContent?.fields?.find { it.label == "Type" }?.value
+    val Places_assises = identificationContent?.fields?.find { it.label == "Places assises" }?.value
+    val age = identificationContent?.fields?.find { it.label == "Âge du véhicule" }?.value
+
+    val motorisationContent = content.find { it.title == "Motorisation"}?.fields
+    val cylindree = motorisationContent?.find { it.label == "Cylindrée" }?.value
+    val puissance = motorisationContent?.find { it.label == "Puissance" }?.value
+    val puissance_fisc = motorisationContent?.find { it.label == "Puissance fiscale" }?.value
+    val cylindres = motorisationContent?.find { it.label == "Cylindres" }?.value
+
+    val carburant = content.find { it.title == "Carburant" }?.fields?.find { it.label == "Type" }?.value
+
+    val first_circ = content.find { it.title == "Historique du véhicule" }?.records?.find { it.name == "Mise en circulation" }?.date
+
+    val boite = content.find { it.title == "Transmission" }?.fields?.find { it.label == "Boîte de vitesses" }?.value
+
+
+    // Constructing the Detail object
+    return Detail(
+        immat = immat,
+        co2 = "119",
+        marque = shortInfo.brand,
+        modele = shortInfo.model,
+        carrosserieCG = Carrosserie,
+        date1erCirFr = first_circ,
+        energie = cylindres,
+        energieNGC = carburant,
+        genreVCG = 1,
+        genreVCGNGC = Type,
+        puisFisc = puissance_fisc,
+        date1erCirUs = first_circ,
+        collection = "non",
+        date30 = "-",
+        vin = vin,
+        boiteVitesse = boite,
+        puisFiscReel = puissance,
+        nrPassagers = Places_assises,
+        nbPortes = "-",
+        typeMine = "-",
+        couleur = "-",
+        poids = "-",
+        cylindres = cylindree,
+        sraId = "-",
+        sraGroup = "-",
+        sraCommercial = "-",
+        logoMarque = "https=//api.apiplaqueimmatriculation.com/logos_marques/fiat.png",
+        codeMoteur = "",
+        kType = "-",
+
+    )
+}
